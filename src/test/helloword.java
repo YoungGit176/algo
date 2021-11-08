3eacc5cf42a4c4d8dbe281e4d5a308e1cf78bfc8
YoungGit176 <1951833487@qq.com>
2021-11-08 15:57:26 +0800

    测试

--- src/test/helloword.java
+++ src/test/helloword.java
@@ -2,6 +2,6 @@ package test;
 
 public class helloword {
     public static void main(String[] args) {
-        System.out.println("hello word");
+        System.out.println("hello word1");
     }
 }
